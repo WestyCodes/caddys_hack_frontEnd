7fe05bbba61b2aca55d182b63162613044b56b0a
Oliver West <oliversimon.west@gmail.com>
2023-07-21 14:49:28 +0100

    adding anchors to contact me

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { AuthProvider, ProtectedRoute } from './context/auth';
-import { ModalProvider } from './context/modal';
+// import { ModalProvider } from './context/modal';
 import HomePage from './pages/home/HomePage.js';
 import LoginPage from './pages/login/LoginPage';
 import RegisterPage from './pages/register/RegisterPage.js';
@@ -12,38 +12,38 @@ import CaddyTrack from './pages/caddyTrack/CaddyTrack.js';
 function App() {
     return (
         <AuthProvider>
-            <ModalProvider>
-                <Routes>
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/register" element={<RegisterPage />} />
+            {/* <ModalProvider> */}
+            <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
 
-                    <Route
-                        path="/dashboard"
-                        element={
-                            <ProtectedRoute>
-                                <Dashboard />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        path="/caddytrack"
-                        element={
-                            <ProtectedRoute>
-                                <CaddyTrack />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        path="/datavis"
-                        element={
-                            <ProtectedRoute>
-                                <DataVis />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route path="/" element={<HomePage />} />
-                </Routes>
-            </ModalProvider>
+                <Route
+                    path="/dashboard"
+                    element={
+                        <ProtectedRoute>
+                            <Dashboard />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="/caddytrack"
+                    element={
+                        <ProtectedRoute>
+                            <CaddyTrack />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="/datavis"
+                    element={
+                        <ProtectedRoute>
+                            <DataVis />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route path="/" element={<HomePage />} />
+            </Routes>
+            {/* </ModalProvider> */}
         </AuthProvider>
     );
 }
